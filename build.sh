bb1179ad19f3613ac174b70be03299f29dce3789
Tom <tom@othermachine.co>
2013-11-12 11:22:29 -0800

    Fix relative path to openmesh in build.sh

--- build.sh
+++ build.sh
@@ -1,2 +1,2 @@
-scons -j7 use_openmesh=1 prefix='#build/$arch/$type' install=0 use_python=0 need_openmesh=1 openmesh_libpath='#/../OpenMesh-2.0/build/Build/lib/OpenMesh/' type=debug 
-scons -j7 use_openmesh=1 prefix='#build/$arch/$type' install=0 use_python=0 need_openmesh=1 openmesh_libpath='#/../OpenMesh-2.0/build/Build/lib/OpenMesh/'
+scons -j7 use_openmesh=1 prefix='#build/$arch/$type' install=0 use_python=0 need_openmesh=1 openmesh_libpath='#/../OpenMesh-2.0/build/Build/lib/OpenMesh/' openmesh_include='#/../OpenMesh-2.0/src/' type=debug
+scons -j7 use_openmesh=1 prefix='#build/$arch/$type' install=0 use_python=0 need_openmesh=1 openmesh_libpath='#/../OpenMesh-2.0/build/Build/lib/OpenMesh/' openmesh_include='#/../OpenMesh-2.0/src/'
