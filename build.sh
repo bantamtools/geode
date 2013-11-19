165a12a878f5f9fdb967b9f2e2de5f65f729b828
Forrest Green <greengadgetz@gmail.com>
2013-11-19 14:07:54 -0800

    Update build.sh to use arch='nocona' for portability to other machines

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 lib_setup_args="use_python=0 use_libjpeg=0 use_libpng=0 use_openexr=0 use_openmesh=1"
 echo "Using: $lib_setup_args"
-scons -j7 prefix='#build/$arch/$type' install=0 $lib_setup_args openmesh_libpath='#/../OpenMesh-2.0/build/Build/lib/OpenMesh/' openmesh_include='#/../OpenMesh-2.0/src/' type=debug 
-scons -j7 prefix='#build/$arch/$type' install=0 $lib_setup_args openmesh_libpath='#/../OpenMesh-2.0/build/Build/lib/OpenMesh/' openmesh_include='#/../OpenMesh-2.0/src/'
+scons -j7 prefix='#build/$arch/$type' arch='nocona' install=0 $lib_setup_args openmesh_libpath='#/../OpenMesh-2.0/build/Build/lib/OpenMesh/' openmesh_include='#/../OpenMesh-2.0/src/' type=debug 
+scons -j7 prefix='#build/$arch/$type' arch='nocona' install=0 $lib_setup_args openmesh_libpath='#/../OpenMesh-2.0/build/Build/lib/OpenMesh/' openmesh_include='#/../OpenMesh-2.0/src/'
