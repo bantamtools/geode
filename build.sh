b3c1234703f6aef91d62b2c6b2466b5082e10aae
Forrest Green <greengadgetz@gmail.com>
2013-11-18 16:20:44 -0800

    Make build.sh not use image libs

--- build.sh
+++ build.sh
@@ -1,2 +1,4 @@
-scons -j7 use_openmesh=1 prefix='#build/$arch/$type' install=0 use_python=0 need_openmesh=1 openmesh_libpath='#/../OpenMesh-2.0/build/Build/lib/OpenMesh/' openmesh_include='#/../OpenMesh-2.0/src/' type=debug
-scons -j7 use_openmesh=1 prefix='#build/$arch/$type' install=0 use_python=0 need_openmesh=1 openmesh_libpath='#/../OpenMesh-2.0/build/Build/lib/OpenMesh/' openmesh_include='#/../OpenMesh-2.0/src/'
+lib_setup_args="use_python=0 use_libjpeg=0 use_libpng=0 use_openexr=0 use_openmesh=1"
+echo "Using: $lib_setup_args"
+scons -j7 prefix='#build/$arch/$type' install=0 $lib_setup_args openmesh_libpath='#/../OpenMesh-2.0/build/Build/lib/OpenMesh/' openmesh_include='#/../OpenMesh-2.0/src/' type=debug 
+scons -j7 prefix='#build/$arch/$type' install=0 $lib_setup_args openmesh_libpath='#/../OpenMesh-2.0/build/Build/lib/OpenMesh/' openmesh_include='#/../OpenMesh-2.0/src/'
