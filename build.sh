f12b267c01e96c03b4b735baea410e5026a396fa
Andrew Hay Kurtz <andrew.hay.kurtz@gmail.com>
2014-05-08 11:56:17 -0700

    add clean support to build.sh

--- build.sh
+++ build.sh
@@ -1,4 +1,8 @@
 lib_setup_args="use_python=0 use_libjpeg=0 use_libpng=0 use_openexr=0 use_openmesh=1"
 echo "Using: $lib_setup_args"
-scons -j7 prefix='#build/$arch/$type' arch='nocona' install=0 $lib_setup_args openmesh_libpath='#/../OpenMesh-2.0/build/Build/lib/OpenMesh/' openmesh_include='#/../OpenMesh-2.0/src/' type=debug 
-scons -j7 prefix='#build/$arch/$type' arch='nocona' install=0 $lib_setup_args openmesh_libpath='#/../OpenMesh-2.0/build/Build/lib/OpenMesh/' openmesh_include='#/../OpenMesh-2.0/src/'
+if [ "$1" = "clean" ] || [ "$1" = "distclean" ]; then
+	scons -c --config=force $lib_setup_args
+else
+	scons -j7 prefix='#build/$arch/$type' arch='nocona' install=0 $lib_setup_args openmesh_libpath='#/../OpenMesh-2.0/build/Build/lib/OpenMesh/' openmesh_include='#/../OpenMesh-2.0/src/' type=debug
+	scons -j7 prefix='#build/$arch/$type' arch='nocona' install=0 $lib_setup_args openmesh_libpath='#/../OpenMesh-2.0/build/Build/lib/OpenMesh/' openmesh_include='#/../OpenMesh-2.0/src/'
+fi
