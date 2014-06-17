055984d472fb94d7f540d92172133b2d721c18a1
Andrew Hay Kurtz <andrew.hay.kurtz@gmail.com>
2014-06-16 17:45:38 -0700

    add explicit path to libgmp.dylib

--- build.sh
+++ build.sh
@@ -3,6 +3,9 @@ echo "Using: $lib_setup_args"
 if [ "$1" = "clean" ] || [ "$1" = "distclean" ]; then
 	scons -c --config=force $lib_setup_args
 else
-	scons -j7 prefix='#build/$arch/$type' arch='nocona' install=0 $lib_setup_args openmesh_libpath='#/../OpenMesh-2.0/build/Build/lib/OpenMesh/' openmesh_include='#/../OpenMesh-2.0/src/' gmp_libpath='#/../gmp/.libs/' gmp_include='#/../gmp/' type=debug
-	scons -j7 prefix='#build/$arch/$type' arch='nocona' install=0 $lib_setup_args openmesh_libpath='#/../OpenMesh-2.0/build/Build/lib/OpenMesh/' openmesh_include='#/../OpenMesh-2.0/src/' gmp_libpath='#/../gmp/.libs/' gmp_include='#/../gmp/'
+  for type in debug release; do
+    scons -j7 prefix='#build/$arch/$type' arch='nocona' install=0 $lib_setup_args \
+      openmesh_libpath='#/../OpenMesh-2.0/build/Build/lib/OpenMesh' openmesh_include='#/../OpenMesh-2.0/src' \
+      gmp_linkflags='../gmp/.libs/libgmp.dylib' gmp_include='#/../gmp' gmp_libpath='' gmp_publiclibs='' type=$type
+  done
 fi
